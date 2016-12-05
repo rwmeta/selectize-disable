cc5260a1bd0d94013c9b66aa688f4a6dfdae28b3
arturgspb <art@realweb.ru>
2016-12-05 14:16:03 +0300

    [-] Убрал параметр invers так как он начал конфликтовать с api элемнетов, которые перестали приходить как строка, а стаки как объект

--- src/selectize-disabled.js
+++ src/selectize-disabled.js
@@ -7,35 +7,19 @@
  *   inverse: if true, a option[fieldName] == false will disable the option (by default option[fieldName] == true disables the option)
  *     (default is false)
  */
-Selectize.define('option-disable', function(options) {
-  var inverseValue = false,
-    fieldName = 'disabled';
-
-  if (options && typeof options.inverse !== 'undefined') {
-    inverseValue = !!options.inverse
-  }
+Selectize.define('option-disable', function (options) {
+  var fieldName = 'disabled';
 
   if (options && typeof options.fieldName !== 'undefined') {
     fieldName = options.fieldName
   }
 
-  // override render to show which elements are disabled
-  var originalRender = this.render;
-  this.render = function(templateName, data) {
-    var html = originalRender.apply(this, arguments);
-    if (data[fieldName] ^ options.inverse) {
-      // first instance of option surrounded by quotes or spaces should be the class
-      html = html.replace(/(['" ])option(?=['" ])/, '$1option selectize-disabled');
-    }
-    return html;
-  }
-
   // override addItem to not add items that are disabled
   var originalAddItem = this.addItem;
-  this.addItem = function(value) {
-    if (this.options.hasOwnProperty(value) && (this.options[value][fieldName] ^ options.inverse))
+  this.addItem = function (value) {
+    if (this.options.hasOwnProperty(value) && (this.options[value][fieldName])) {
       return;
-    else
-      originalAddItem.apply(this, arguments);
+    }
+    originalAddItem.apply(this, arguments);
   }
 });
